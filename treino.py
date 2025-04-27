377c555409beede3e967e3af448e0e0163bcb503
samucj73 <samu.rcj@gmail.com>
2025-04-27 19:03:14 -0300

    Update treino.py

--- treino.py
+++ treino.py
@@ -17,81 +17,100 @@ def gerar_treino(usuario):
     treino += "## Treino Semanal\n"
 
     if objetivo == "emagrecimento":
-        treino += """
-**Treino A (Peito e Tríceps)**  
-- Supino reto com halteres (3x15)  
-- Crossover no cabo (3x20)  
-- Tríceps corda (3x20)  
-
-**Treino B (Costas e Bíceps)**  
-- Puxada frente aberta (3x15)  
-- Remada baixa (3x15)  
-- Rosca direta (3x20)  
-
-**Treino C (Pernas e Abdômen)**  
-- Agachamento livre (3x15)  
-- Leg press (3x20)  
-- Abdominal prancha (3x30s)  
-
-**Treino D (Ombros)**  
-- Desenvolvimento com halteres (3x15)  
-- Elevação lateral (3x20)  
-
-**Treino E (Cardio/Funcional)**  
-- Corrida/caminhada (30 minutos)  
-- Circuito funcional (20 minutos)  
-"""
+        cardio = "**Treino E (Cardio/Funcional)**\n- Corrida moderada (40 minutos)\n- Circuito funcional (30 minutos)\n"
+        series_reps = "(3x20)"
     elif objetivo == "hipertrofia":
-        treino += """
-**Treino A (Peito e Tríceps)**  
-- Supino reto barra (4x8)  
-- Supino inclinado halteres (4x10)  
-- Tríceps francês (4x12)  
-
-**Treino B (Costas e Bíceps)**  
-- Barra fixa assistida (4x8)  
-- Remada unilateral (4x10)  
-- Rosca alternada (4x12)  
-
-**Treino C (Pernas e Abdômen)**  
-- Agachamento livre (4x8)  
-- Leg press (4x10)  
-- Stiff (4x10)  
-- Abdominal infra solo (3x20)  
-
-**Treino D (Ombro e Trapézio)**  
-- Desenvolvimento militar (4x8)  
-- Elevação frontal (4x10)  
-- Encolhimento trapézio (4x12)  
-
-**Treino E (Cardio leve)**  
-- Bicicleta ergométrica (20 minutos)  
-"""
+        cardio = "**Treino E (Cardio leve)**\n- Bicicleta ergométrica (20 minutos)\n- Caminhada leve (20 minutos)\n"
+        series_reps = "(4x8-12)"
     else:  # resistência
-        treino += """
-**Treino A (Peito e Tríceps)**  
-- Supino máquina (3x20)  
-- Crossover leve (3x20)  
-- Tríceps pulley (3x25)  
+        cardio = "**Treino E (Cardio longo)**\n- Caminhada intensa (45 minutos)\n- Corrida leve (30 minutos)\n"
+        series_reps = "(3x20-25)"
 
-**Treino B (Costas e Bíceps)**  
-- Puxada frente leve (3x20)  
-- Remada máquina (3x20)  
-- Rosca martelo (3x25)  
+    # Treino A: Peito e Tríceps
+    treino += """
+**Treino A (Peito e Tríceps)**
 
-**Treino C (Pernas e Abdômen)**  
-- Cadeira extensora (3x20)  
-- Mesa flexora (3x20)  
-- Abdominal oblíquo (3x30)  
+**Peito:**
+- Supino reto com barra - Máquina/Banco {}
+- Supino inclinado com halteres - Halteres/Banco {}
+- Crossover no cabo - Crossover/Cabo {}
+- Crucifixo reto com halteres - Halteres/Banco {}
+- Flexão de braços no solo - Peso corporal {}
+- Supino declinado com barra - Máquina/Banco {}
 
-**Treino D (Ombro e Core)**  
-- Elevação lateral leve (3x20)  
-- Prancha isométrica (3x30s)  
+**Tríceps:**
+- Tríceps testa com barra EZ - Barra/Anilha {}
+- Tríceps pulley com corda - Cabo {}
+- Tríceps francês unilateral - Halteres {}
+- Tríceps mergulho em banco - Peso corporal {}
+- Tríceps coice com halteres - Halteres {}
+- Tríceps banco máquina - Máquina {}
+""".format(series_reps, series_reps, series_reps, series_reps, series_reps, series_reps,
+           series_reps, series_reps, series_reps, series_reps, series_reps, series_reps)
 
-**Treino E (Cardio longo)**  
-- Caminhada intensa (40 minutos)  
-"""
+    # Treino B: Costas e Bíceps
+    treino += """
+**Treino B (Costas e Bíceps)**
 
+**Costas:**
+- Puxada frente aberta - Máquina/Cabo {}
+- Remada curvada com barra - Barra {}
+- Remada baixa - Máquina/Cabo {}
+- Remada unilateral com halteres - Halteres {}
+- Puxada neutra (pegada fechada) - Máquina/Cabo {}
+- Pullover na máquina - Máquina {}
+
+**Bíceps:**
+- Rosca direta barra EZ - Barra {}
+- Rosca alternada - Halteres {}
+- Rosca concentrada - Halteres {}
+- Rosca martelo - Halteres {}
+- Rosca scott máquina - Máquina {}
+- Rosca inversa com barra - Barra {}
+""".format(series_reps, series_reps, series_reps, series_reps, series_reps, series_reps,
+           series_reps, series_reps, series_reps, series_reps, series_reps, series_reps)
+
+    # Treino C: Pernas + Abdômen
+    treino += """
+**Treino C (Pernas e Abdômen)**
+
+**Pernas (Quadríceps, Posterior, Glúteo):**
+- Agachamento livre - Barra {}
+- Leg press 45° - Máquina {}
+- Cadeira extensora - Máquina {}
+- Mesa flexora - Máquina {}
+- Stiff com halteres - Halteres {}
+- Avanço (passada) com halteres - Halteres {}
+
+**Abdômen:**
+- Prancha isométrica - Peso corporal (3x30s)
+- Abdominal supra solo - Peso corporal (3x30)
+- Abdominal oblíquo com halteres - Halteres (3x20)
+""".format(series_reps, series_reps, series_reps, series_reps, series_reps, series_reps)
+
+    # Treino D: Ombro e Trapézio
+    treino += """
+**Treino D (Ombro e Trapézio)**
+
+**Ombro:**
+- Desenvolvimento militar com barra - Barra {}
+- Desenvolvimento com halteres - Halteres {}
+- Elevação lateral com halteres - Halteres {}
+- Elevação frontal com halteres - Halteres {}
+- Crucifixo inverso no peck deck - Máquina {}
+- Remada alta com barra - Barra {}
+
+**Trapézio:**
+- Encolhimento de ombros com halteres - Halteres {}
+- Encolhimento com barra - Barra {}
+- Remada alta pegada fechada - Barra {}
+- Trapézio máquina - Máquina {}
+- Remada alta no cross-over - Cabo {}
+- Shrug no smith - Máquina Smith {}
+""".format(series_reps, series_reps, series_reps, series_reps, series_reps, series_reps,
+           series_reps, series_reps, series_reps, series_reps, series_reps, series_reps)
+
+    treino += "\n" + cardio
     treino += "\n---\n"
     treino += "_Recomendamos avaliação médica antes de iniciar atividades físicas._"
 
