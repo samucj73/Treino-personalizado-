fd4ab27ffbf49b182c4046e1136ad0d259ad5da4
samucj73 <samu.rcj@gmail.com>
2025-04-28 14:59:51 -0300

    Update usuario.py

--- usuario.py
+++ usuario.py
@@ -1,14 +1,13 @@
 from db import cadastrar_usuario, obter_usuario, atualizar_usuario
 
 # Função para cadastro de usuário
-def cadastrar(nome, senha, idade, peso, altura, genero, objetivo, experiencia):
-    cadastrar_usuario(nome, senha, idade, peso, altura, genero, objetivo, experiencia)
+def cadastrar(nome, senha, idade, peso, altura, genero, objetivo, experiencia, dias_treino):
+    cadastrar_usuario(nome, senha, idade, peso, altura, genero, objetivo, experiencia, dias_treino)
 
 # Função para buscar o usuário no banco
 def obter(nome, senha):
     return obter_usuario(nome, senha)
 
 # Função para atualizar o perfil do usuário
-def atualizar_perfil(nome, idade, peso, altura, genero, objetivo, experiencia, dias_treino):
-    # Atualizando os dados do usuário no banco
+def atualizar(nome, idade, peso, altura, genero, objetivo, experiencia, dias_treino):
     atualizar_usuario(nome, idade, peso, altura, genero, objetivo, experiencia, dias_treino)
