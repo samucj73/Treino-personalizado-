b6058ea940942eb0c700057a16289ced3af46fb7
samucj73 <samu.rcj@gmail.com>
2025-04-29 13:20:02 -0300

    Update usuario.py

--- usuario.py
+++ usuario.py
@@ -1,8 +1,8 @@
-from db import cadastrar_usuario, obter_usuario, atualizar_usuario
+from db import cadastrar_usuario, obter_usuario, atualizar_usuario, recuperar_por_email
 
-def cadastrar(nome, senha, idade, peso, altura, genero, objetivo, experiencia, dias_treino):
+def cadastrar(nome, email, senha, idade, peso, altura, genero, objetivo, experiencia, dias_treino):
     try:
-        cadastrar_usuario(nome, senha, idade, peso, altura, genero, objetivo, experiencia, dias_treino)
+        cadastrar_usuario(nome, email, senha, idade, peso, altura, genero, objetivo, experiencia, dias_treino)
     except Exception as e:
         raise Exception(f"Erro ao cadastrar: {e}")
 
@@ -17,3 +17,9 @@ def atualizar(id_usuario, nome, idade, peso, altura, genero, objetivo, experienc
         atualizar_usuario(id_usuario, nome, idade, peso, altura, genero, objetivo, experiencia, dias_treino)
     except Exception as e:
         raise Exception(f"Erro ao atualizar usuário: {e}")
+
+def recuperar_credencial(email):
+    try:
+        return recuperar_por_email(email)
+    except Exception as e:
+        raise Exception(f"Erro ao recuperar credenciais: {e}")
