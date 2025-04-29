f297d21dbaf6093f38fbb3184f6eea547de73a92
samucj73 <samu.rcj@gmail.com>
2025-04-29 00:41:22 -0300

    Update usuario.py

--- usuario.py
+++ usuario.py
@@ -4,17 +4,16 @@ def cadastrar(nome, senha, idade, peso, altura, genero, objetivo, experiencia, d
     try:
         cadastrar_usuario(nome, senha, idade, peso, altura, genero, objetivo, experiencia, dias_treino)
     except Exception as e:
-        print(f"Erro ao cadastrar: {e}")
+        raise Exception(f"Erro ao cadastrar: {e}")
 
 def obter(nome, senha):
     try:
         return obter_usuario(nome, senha)
     except Exception as e:
-        print(f"Erro ao obter usuário: {e}")
-        return None
+        raise Exception(f"Erro ao obter usuário: {e}")
 
 def atualizar(id_usuario, nome, idade, peso, altura, genero, objetivo, experiencia, dias_treino):
     try:
         atualizar_usuario(id_usuario, nome, idade, peso, altura, genero, objetivo, experiencia, dias_treino)
     except Exception as e:
-        print(f"Erro ao atualizar usuário: {e}")
+        raise Exception(f"Erro ao atualizar usuário: {e}")
